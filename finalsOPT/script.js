2c538d13d189dfb58ed4491bbc4d8e8ff6fd86a4
John Paul Patigas <johnpaulpatigas@gmail.com>
2023-12-11 21:01:56 +0800

    Uhh spaces or tabs

--- finalsOPT/script.js
+++ finalsOPT/script.js
@@ -8,10 +8,10 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
 	var validPassword = "1234";
 
 	if (username === validUsername && password === validPassword) {
-    	document.getElementById("message").innerHTML = "Login successful!";
-    	window.location.href = "portfolio/index.html";
+		document.getElementById("message").innerHTML = "Login successful!";
+		window.location.href = "portfolio/index.html";
   	} else {
-    	document.getElementById("message").innerHTML =
-      	"Invalid username or password. Please try again.";
+		document.getElementById("message").innerHTML =
+		"Invalid username or password. Please try again.";
   	}
 });
