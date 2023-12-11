fe9b78ba7bb6c3988befd4c1815eb68d9d48967b
John Paul Patigas <johnpaulpatigas@gmail.com>
2023-12-11 21:04:25 +0800

    Semicolon go away

--- finalsOPT/script.js
+++ finalsOPT/script.js
@@ -1,17 +1,17 @@
 document.getElementById("loginForm").addEventListener("submit", function (e) {
-	e.preventDefault();
+	e.preventDefault()
 
-	var username = document.getElementById("username").value;
-	var password = document.getElementById("password").value;
+	var username = document.getElementById("username").value
+	var password = document.getElementById("password").value
 
-	var validUsername = "user";
-	var validPassword = "1234";
+	var validUsername = "user"
+	var validPassword = "1234"
 
 	if (username === validUsername && password === validPassword) {
-		document.getElementById("message").innerHTML = "Login successful!";
+		document.getElementById("message").innerHTML = "Login successful!"
 		window.location.href = "portfolio/index.html";
   	} else {
 		document.getElementById("message").innerHTML =
-		"Invalid username or password. Please try again.";
+		"Invalid username or password. Please try again."
   	}
-});
+})
