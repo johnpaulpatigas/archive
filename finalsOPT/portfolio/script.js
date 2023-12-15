6f0e8b71c87b23a5e4b6ff879c78c877e3279dad
John Paul Patigas <johnpaulpatigas@gmail.com>
2023-12-15 13:36:41 +0800

    I am inevitable

--- finalsOPT/portfolio/script.js
+++ finalsOPT/portfolio/script.js
@@ -8,3 +8,15 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
         })
     })
 })
+
+const container = document.getElementById("main");
+
+container.addEventListener("wheel", function (e) {
+    if (e.deltaY > 0) {
+        container.scrollLeft += 100;
+        e.preventDefault();
+    } else {
+        container.scrollLeft -= 100;
+        e.preventDefault();
+    }
+});
