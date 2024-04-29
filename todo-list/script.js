6fe4e230b1e06a0ff5738949eb567969f6c0318a
John Paul Patigas <johnpaulpatigas@gmail.com>
2024-04-29 15:41:44 +0800

    todo-list: JavaScript go brr

---
+++ todo-list/script.js
@@ -0,0 +1,29 @@
+const taskForm = document.getElementById("task-form");
+const taskList = document.getElementById("task-list");
+
+function createTaskItem(task) {
+  const li = document.createElement("li");
+  li.className = "list-group-item";
+
+  const title = document.createElement("h5");
+  title.innerText = task.title;
+
+  const description = document.createElement("p");
+  description.innerText = task.description;
+
+  const savedDate = document.createElement("span");
+  savedDate.classList.add("text-muted");
+  savedDate.innerText = `Saved: ${new Date().toLocaleDateString()}`;
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("btn", "btn-sm", "btn-danger", "float-end");
+  deleteBtn.innerText = "Delete";
+  deleteBtn.addEventListener("click", () => removeTask(task));
+
+  li.appendChild(title);
+  li.appendChild(description);
+  li.appendChild(savedDate);
+  li.appendChild(deleteBtn);
+
+  return li;
+}
