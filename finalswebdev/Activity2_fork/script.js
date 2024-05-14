ab8f09f17c3587515800de4e523cf4449257d2a3
John Paul Patigas <johnpaulpatigas@gmail.com>
2024-05-14 18:26:26 +0800

    It's not a spoon, but a fork

---
+++ finalswebdev/Activity2_fork/script.js
@@ -0,0 +1,40 @@
+const questions = [
+    { q: "1. What is the capital of Philippines?", a: "Manila" },
+    { q: "2. Who is the current Philippine president?", a: "Bongbong Marcos" },
+    { q: "3. Where is Philippines located?", a: "Asia" },
+    { q: "4. What is the Queen City of the South?", a: "Cebu City" },
+    {
+        q: "5. Where did Lapu-Lapu vs. Magellan's fight take place?",
+        a: "Mactan",
+    },
+];
+
+let i = 0;
+
+function display() {
+    document.getElementById("question").textContent = questions[i].q;
+}
+
+function check() {
+    const answer = document.getElementById("answer").value;
+    if (answer == questions[i].a) {
+        i++;
+        if (i == questions.length) {
+            alert("Congrats! You answered all questions correctly!");
+        } else {
+            display();
+            document.getElementById("feedback-correct").textContent =
+                "Correct! Next question...";
+            document.getElementById("feedback-wrong").textContent = "";
+        }
+    } else {
+        display();
+        document.getElementById("feedback-correct").textContent = "";
+        document.getElementById("feedback-wrong").textContent =
+            "Incorrect. Please try again.";
+    }
+    document.getElementById("answer").value = "";
+}
+
+display();
+document.getElementById("submit").addEventListener("click", check);
